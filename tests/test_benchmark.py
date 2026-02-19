f799d0d55aec6f9d6b5ec570796f7a9164c3a4a6
Sander Jonkers <san.der.jonkers+github@gmail.com>
2026-02-19 15:17:10 +0100

    benchmark calculation to MB/s

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,5 +1,8 @@
 from tests.testsupport import *
 
+def test_print_sabctools_version(): 
+    print(f"sabctools version: {sabctools.version}")
+
 def test_sabctools_decode(benchmark):
     data_plain = read_plain_yenc_file("test_regular.yenc")
     benchmark(sabctools_yenc_wrapper, data_plain)
